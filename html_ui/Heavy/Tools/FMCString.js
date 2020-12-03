58298600409bfd64418fb40a690078f3d6c8d819
김라댜 <43503767+kimlada@users.noreply.github.com>
2020-12-03 20:53:16 +0100

    added basic characters

--- html_ui/Heavy/Tools/FMCString.js
+++ html_ui/Heavy/Tools/FMCString.js
@@ -3,10 +3,35 @@ const FMCString = {
 	PageTitle: {},
 	LineTitle: {},
 	Line: {
-		Dash: {},
-		Box: {}
+		Dash: {
+			1: '-',
+			2: '--',
+			3: '---',
+			5: '-----',
+		},
+		Box: {
+			1: '□',
+			2: '□□',
+			3: '□□□',
+			4: '□□□□',
+			5: '□□□□□'
+		},
+		Underscore: {
+			1: '_',
+			2: '__',
+			3: '___',
+			4: '____',
+			5: '_____'
+		}
 	},
 	Scratchpad: {},
 	Prompt: {},
-	Common: {}
+	Common: {
+		SLASH: '/',
+		BACKSLASH: '\\',
+		PERIOD: '.',
+		COMMA: ',',
+		BOX: '□',
+		FMCSEPARATOR: '------------------------'
+	}
 };
