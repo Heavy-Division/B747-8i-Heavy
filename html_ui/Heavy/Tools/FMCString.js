cc8efda070f4e2e4151ecf15d109b35f676dcbd2
김라댜 <43503767+kimlada@users.noreply.github.com>
2020-12-03 21:14:26 +0100

    added basic strings for VNAV pages

--- html_ui/Heavy/Tools/FMCString.js
+++ html_ui/Heavy/Tools/FMCString.js
@@ -1,7 +1,20 @@
 const FMCString = {
 	Placeholder: {},
-	PageTitle: {},
-	LineTitle: {},
+	PageTitle: {
+		ECON: 'ECON',
+		LIM_SPD: 'LIM SPD',
+		MCP_SPD: 'MCP SPD',
+		E_O: 'E/O'
+	},
+	LineTitle: {
+		CRZ_ALT: 'CRZ ALT',
+		ECON_SPD: 'ECON SPD',
+		SEL_SPD: 'SEL SPD',
+		SPD_TRANS: 'SPD TRANS',
+		SPD_RESTR: 'SPD RESTR',
+		TRANS_ALT: 'TRANS ALT',
+		MAX_ANGLE: 'MAX ANGLE'
+	},
 	Line: {
 		Dash: {
 			1: '-',
@@ -25,13 +38,18 @@ const FMCString = {
 		}
 	},
 	Scratchpad: {},
-	Prompt: {},
+	Prompt: {
+		ECON_LEFT: '<ECON',
+		ENG_OUT_RIGHT: 'ENG OUT>',
+		CLB_DIR_RIGHT: 'CLB DIR>'
+	},
 	Common: {
 		SLASH: '/',
 		BACKSLASH: '\\',
 		PERIOD: '.',
 		COMMA: ',',
 		BOX: '□',
-		FMCSEPARATOR: '------------------------'
+		FMC_SEPARATOR: '------------------------',
+		FLIGHT_LEVEL: 'FL'
 	}
 };
