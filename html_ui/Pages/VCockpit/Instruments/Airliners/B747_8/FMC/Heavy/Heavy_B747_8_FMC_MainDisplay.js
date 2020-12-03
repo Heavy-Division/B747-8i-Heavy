0c03497777cf57298928176837862e3455eff3d2
김라댜 <43503767+kimlada@users.noreply.github.com>
2020-12-03 01:36:29 +0100

    unused imports removed

---
+++ html_ui/Pages/VCockpit/Instruments/Airliners/B747_8/FMC/Heavy/Heavy_B747_8_FMC_MainDisplay.js
@@ -0,0 +1,19 @@
+class Heavy_B747_8_FMC_MainDisplay extends B747_8_FMC_MainDisplay {
+
+    constructor() {
+        super(...arguments)
+    }
+
+    Init() {
+        super.Init()
+        this.registerMainButtonsActions()
+    }
+
+    registerMainButtonsActions() {
+        this.onVNAV = () => {
+            Heavy_B747_8_FMC_VNAVPage.ShowPage1(this)
+        }
+    }
+}
+
+registerInstrument("fmc-b747-8-main-display", Heavy_B747_8_FMC_MainDisplay)
