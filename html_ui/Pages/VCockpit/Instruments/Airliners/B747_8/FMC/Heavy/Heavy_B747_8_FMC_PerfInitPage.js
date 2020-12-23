76a4b622eab3febe2eb2f6ce516da44d33d87870
김라댜 <43503767+kimlada@users.noreply.github.com>
2020-12-23 18:44:58 +0100

    B747_8_FMC_PerfInitPage - Updated Official

--- html_ui/Pages/VCockpit/Instruments/Airliners/B747_8/FMC/Heavy/Heavy_B747_8_FMC_PerfInitPage.js
+++ html_ui/Pages/VCockpit/Instruments/Airliners/B747_8/FMC/Heavy/Heavy_B747_8_FMC_PerfInitPage.js
@@ -1,86 +1,100 @@
-class Heavy_B747_8_FMC_PerfInitPage {
+class FMCPerfInitPage {
 	static ShowPage1(fmc) {
-		fmc.clearDisplay();
-		fmc.updateFuelVars();
-		Heavy_B747_8_FMC_PerfInitPage._timer = 0;
-		fmc.pageUpdate = () => {
-			Heavy_B747_8_FMC_PerfInitPage._timer++;
-			if (Heavy_B747_8_FMC_PerfInitPage._timer >= 15) {
-				Heavy_B747_8_FMC_PerfInitPage.ShowPage1(fmc);
-			}
-		};
-		let grossWeightCell = FMCString.Line.Box['3'] + FMCString.Common.PERIOD + FMCString.Line.Box['1'];
-		if (isFinite(fmc.getWeight(true))) {
-			grossWeightCell = fmc.getWeight(true).toFixed(0);
-		}
-		fmc.onLeftInput[0] = () => {
-			let value = fmc.inOut;
-			fmc.clearUserInput();
-			fmc.setWeight(value, result => {
-				if (result) {
+		fmc.updateFuelVars().then(() => {
+			fmc.clearDisplay();
+			Heavy_B747_8_FMC_PerfInitPage._timer = 0;
+			fmc.pageUpdate = () => {
+				Heavy_B747_8_FMC_PerfInitPage._timer++;
+				if (Heavy_B747_8_FMC_PerfInitPage._timer >= 15) {
 					Heavy_B747_8_FMC_PerfInitPage.ShowPage1(fmc);
 				}
-			}, true);
-		};
-		let crzAltCell = FMCString.Line.Box['5'];
-		if (isFinite(fmc.cruiseFlightLevel)) {
-			crzAltCell = fmc.cruiseFlightLevel.toFixed(0);
-		}
-		fmc.onRightInput[0] = () => {
-			let value = fmc.inOut;
-			fmc.clearUserInput();
-			if (fmc.setCruiseFlightLevelAndTemperature(value)) {
-				Heavy_B747_8_FMC_PerfInitPage.ShowPage1(fmc);
+			};
+			let grossWeightCell = FMCString.Line.Box['3'] + FMCString.Common.PERIOD + FMCString.Line.Box['1'];
+			if (isFinite(fmc.getFuelVarsUpdatedGrossWeight(true))) {
+				grossWeightCell = fmc.getFuelVarsUpdatedGrossWeight(true).toFixed(1) + ' lb';
 			}
-		};
-		let blockFuelCell = FMCString.Line.Box['3'] + FMCString.Common.PERIOD + FMCString.Line.Box['1'];
-		if (isFinite(fmc.getBlockFuel(true))) {
-			blockFuelCell = fmc.getBlockFuel(true).toFixed(1) + ' lb';
-		}
-		let zeroFuelWeightCell = FMCString.Line.Box['3'] + FMCString.Common.PERIOD + FMCString.Line.Box['1'];
-		if (isFinite(fmc.getZeroFuelWeight(true))) {
-			zeroFuelWeightCell = fmc.getZeroFuelWeight(true).toFixed(1);
-		}
-		fmc.onLeftInput[2] = () => {
-			let value = fmc.inOut;
-			fmc.clearUserInput();
-			if (fmc.trySetZeroFuelWeightZFWCG(value, true)) {
-				Heavy_B747_8_FMC_PerfInitPage.ShowPage1(fmc);
+			fmc.onLeftInput[0] = () => {
+				let value = fmc.inOut;
+				fmc.clearUserInput();
+				fmc.setWeight(value, result => {
+					if (result) {
+						Heavy_B747_8_FMC_PerfInitPage.ShowPage1(fmc);
+					}
+				}, true);
+			};
+			let crzAltCell = FMCString.Line.Box['5'];
+			if (isFinite(fmc.cruiseFlightLevel)) {
+				crzAltCell = fmc.cruiseFlightLevel.toFixed(0);
 			}
-		};
-		let costIndex = FMCString.Line.Box['3'];
-		if (fmc.costIndex) {
-			costIndex = fmc.costIndex + '[color]blue';
-		}
-		fmc.onRightInput[1] = () => {
-			let value = fmc.inOut;
-			fmc.clearUserInput();
-			if (fmc.tryUpdateCostIndex(value)) {
-				Heavy_B747_8_FMC_PerfInitPage.ShowPage1(fmc);
+			fmc.onRightInput[0] = () => {
+				let value = fmc.inOut;
+				fmc.clearUserInput();
+				if (fmc.setCruiseFlightLevelAndTemperature(value)) {
+					Heavy_B747_8_FMC_PerfInitPage.ShowPage1(fmc);
+				}
+			};
+			let blockFuelCell = FMCString.Line.Box['3'] + FMCString.Common.PERIOD + FMCString.Line.Box['1'];
+			if (isFinite(fmc.getBlockFuel(true))) {
+				blockFuelCell = fmc.getBlockFuel(true).toFixed(1) + ' lb';
 			}
-		};
-		fmc.setTemplate([
-			['PERF INIT'],
-			['GR WT', 'CRZ ALT'],
-			[grossWeightCell, crzAltCell],
-			['FUEL', 'COST INDEX'],
-			[blockFuelCell, costIndex],
-			['ZFW', 'MIN FUEL TEMP'],
-			[zeroFuelWeightCell, '-37°c'],
-			['RESERVES', 'CRZ CG'],
-			[FMCString.Line.Box['3'] + FMCString.Common.PERIOD + FMCString.Line.Box['1'], '20.0%'],
-			['DATA LINK', 'STEP SIZE'],
-			['NO COMM', 'RVSM'],
-			[FMCString.Common.FMC_SEPARATOR],
-			[FMCString.Prompt.INDEX_LEFT, FMCString.Prompt.THRUST_LIM_RIGHT]
-		]);
-		fmc.onLeftInput[5] = () => {
-			Heavy_B747_8_FMC_InitRefIndexPage.ShowPage1(fmc);
-		};
-		fmc.onRightInput[5] = () => {
-			Heavy_B747_8_FMC_ThrustLimPage.ShowPage1(fmc);
-		};
+			let zeroFuelWeightCell = FMCString.Line.Box['3'] + FMCString.Common.PERIOD + FMCString.Line.Box['1'];
+			if (isFinite(fmc.getZeroFuelWeight(true))) {
+				zeroFuelWeightCell = fmc.getZeroFuelWeight(true).toFixed(1) + ' lb';
+			}
+			fmc.onLeftInput[2] = () => {
+				let value = fmc.inOut;
+				fmc.clearUserInput();
+				if (fmc.trySetZeroFuelWeightZFWCG(value, true)) {
+					Heavy_B747_8_FMC_PerfInitPage.ShowPage1(fmc);
+				}
+			};
+			let costIndex = FMCString.Line.Box['4'];
+			if (isFinite(fmc.costIndex)) {
+				costIndex = fmc.costIndex.toFixed(0);
+			}
+			fmc.onRightInput[1] = () => {
+				let value = fmc.inOut;
+				fmc.clearUserInput();
+				if (fmc.tryUpdateCostIndex(value, 10000)) {
+					Heavy_B747_8_FMC_PerfInitPage.ShowPage1(fmc);
+				}
+			};
+			let reservesCell = FMCString.Line.Box['3'] + FMCString.Common.PERIOD + FMCString.Line.Box['1'];
+			let reserves = fmc.getFuelReserves();
+			if (isFinite(reserves)) {
+				reservesCell = reserves.toFixed(1) + ' lb';
+			}
+			fmc.onLeftInput[3] = () => {
+				let value = fmc.inOut;
+				fmc.clearUserInput();
+				if (fmc.setFuelReserves(value, true)) {
+					Heavy_B747_8_FMC_PerfInitPage.ShowPage1(fmc);
+				}
+			};
+			fmc.setTemplate([
+				['PERF INIT'],
+				['GR WT', 'CRZ ALT'],
+				[grossWeightCell, crzAltCell],
+				['FUEL', 'COST INDEX'],
+				[blockFuelCell, costIndex],
+				['ZFW', 'MIN FUEL TEMP'],
+				[zeroFuelWeightCell, '-37°c'],
+				['RESERVES', 'CRZ CG'],
+				[reservesCell, '20.0%'],
+				['DATA LINK', 'STEP SIZE'],
+				['NO COMM', 'RVSM'],
+				[FMCString.Common.FMC_SEPARATOR],
+				[FMCString.Prompt.INDEX_LEFT, FMCString.Prompt.THRUST_LIM_RIGHT]
+			]);
+			fmc.onLeftInput[5] = () => {
+				B747_8_FMC_InitRefIndexPage.ShowPage1(fmc);
+			};
+			fmc.onRightInput[5] = () => {
+				Heavy_B747_8_FMC_ThrustLimPage.ShowPage1(fmc);
+			};
+		});
 	}
 }
 
 Heavy_B747_8_FMC_PerfInitPage._timer = 0;
+//# sourceMappingURL=B747_8_FMC_PerfInitPage.js.map
